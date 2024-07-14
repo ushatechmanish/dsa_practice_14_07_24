9a012a702c9f659ed85f1837a532c856520cace2
Manish Chauhan <manish.chauhan@ushatech.in>
2024-07-14 11:01:32 +0530

    searchInArray

--- src/main/java/in/ushatech/dsa/DsaApplication.java
+++ src/main/java/in/ushatech/dsa/DsaApplication.java
@@ -160,4 +160,12 @@ public class DsaApplication {
 		}
 		return new int[] { -1, -1 };
 	}
+
+	public int searchInArray(int[] intArray, int valueToSearch) {
+		for(int i=0 ; i< intArray.length ; ++i)
+		{
+			if(intArray[i]==valueToSearch) return i;
+		}
+		return -1;
+	}
 }
