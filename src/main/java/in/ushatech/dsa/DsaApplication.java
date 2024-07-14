75dd003deb11f647195f879778cac8b1e6a8cc75
Manish Chauhan <manish.chauhan@ushatech.in>
2024-07-14 10:59:50 +0530

    twoSum

--- src/main/java/in/ushatech/dsa/DsaApplication.java
+++ src/main/java/in/ushatech/dsa/DsaApplication.java
@@ -15,7 +15,7 @@ public class DsaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsaApplication.class, args);
-		System.out.println("removeDuplicates"+removeDuplicates(new int[]{1,1,2}));
+		System.out.println("removeDuplicates" + removeDuplicates(new int[] { 1, 1, 2 }));
 	}
 
 	public static int[] middle(int[] input) {
@@ -92,62 +92,72 @@ public class DsaApplication {
 		return (int) (sumFrom1ToN - sum);
 	}
 
-	public static int removeDuplicates(int[] nums) 
-    {
-        // System.out.println(Arrays.toString(nums));
-        if(nums==null || nums.length==0)
-        {
-            return 0;
-        }
-        if(nums.length==1)
-        {
-            return 1;
-        }
-        
-        // int uniqueCount=0; This is wrong as the iteration is starting from 2nd number
-		//  Dry run was not done for the algorithm 
-        int uniqueCount=1;
-        int prevNumber=nums[0];
-        for(int i=1; i< nums.length; ++i)
-        {
-            if(nums[i]==prevNumber)
-            {
-                continue;
-            }
-            prevNumber=nums[i];
-            ++uniqueCount;
-        }
-        
-        return uniqueCount;
-    }
+	public static int removeDuplicates(int[] nums) {
+		// System.out.println(Arrays.toString(nums));
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		if (nums.length == 1) {
+			return 1;
+		}
 
-	public int maxProfit(int[] prices) //[7, 1, 5, 3, 6, 4] 
-    {
-        int currentProfit = 0; // 2
-        int maxProfit=0; // 4
-        int currentMin = prices[0]; // 1
-        
-        for(int i=1; i< prices.length ; ++i) // i=3
-        {
-            int price = prices[i]; // 3
-            if(price>currentMin) // profit exists // 3>1 true
-            {
-                currentProfit=price-currentMin; // 3-1=2 
-                if(currentProfit>maxProfit) // false
-                {
-                    maxProfit=currentProfit;
-                    continue;
-                }
-            }
-            
-            if(price<currentMin) // 1<7 false
-            {
-                currentMin=price; 
-            }
-            
-        }
-        
-        return maxProfit;
-            
-    }
+		// int uniqueCount=0; This is wrong as the iteration is starting from 2nd number
+		// Dry run was not done for the algorithm
+		int uniqueCount = 1;
+		int prevNumber = nums[0];
+		for (int i = 1; i < nums.length; ++i) {
+			if (nums[i] == prevNumber) {
+				continue;
+			}
+			prevNumber = nums[i];
+			++uniqueCount;
+		}
+
+		return uniqueCount;
+	}
+
+	public int maxProfit(int[] prices) // [7, 1, 5, 3, 6, 4]
+	{
+		int currentProfit = 0; // 2
+		int maxProfit = 0; // 4
+		int currentMin = prices[0]; // 1
+
+		for (int i = 1; i < prices.length; ++i) // i=3
+		{
+			int price = prices[i]; // 3
+			if (price > currentMin) // profit exists // 3>1 true
+			{
+				currentProfit = price - currentMin; // 3-1=2
+				if (currentProfit > maxProfit) // false
+				{
+					maxProfit = currentProfit;
+					continue;
+				}
+			}
+
+			if (price < currentMin) // 1<7 false
+			{
+				currentMin = price;
+			}
+
+		}
+
+		return maxProfit;
+
+	}
+
+	public int[] twoSum(int[] nums, int target) {
+		if (nums == null || nums.length < 2)
+			return new int[] { -1, -1 };
+
+		for (int i = 0; i < nums.length; ++i) {
+			int current = nums[i];
+			int required = target - current;
+			for (int j = i + 1; j < nums.length; ++j) {
+				if (nums[j] == required)
+					return new int[] { i, j };
+			}
+		}
+		return new int[] { -1, -1 };
+	}
 }
