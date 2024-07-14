cbdbf650b9f3ac367933654d10f94cad62b1421f
Manish Chauhan <manish.chauhan@ushatech.in>
2024-07-14 08:53:46 +0530

    first commit

---
+++ src/main/java/in/ushatech/dsa/DsaApplication.java
@@ -0,0 +1,27 @@
+package in.ushatech.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaApplication.class, args);
+		
+	}
+
+	public static int[] middle(int[] input)
+    {
+        int[] result = new int[input.length-2];
+        for(int i=0 ; i< input.length; ++i)
+        {
+            if(i==0 || i== input.length-1)
+            {
+                continue;
+            }
+            result[i-1]=input[i];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/in/ushatech/dsa/DsaApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ushatech.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
