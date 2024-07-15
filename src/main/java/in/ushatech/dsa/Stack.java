ef34aca9dbd87cecba18a82150a6d7a0f0e153f8
Manish Chauhan <manish.chauhan@ushatech.in>
2024-07-15 22:21:32 +0530

    Implemented push method in Stack class . Learning : Add a main method to test the changes locally .

--- src/main/java/in/ushatech/dsa/Stack.java
+++ src/main/java/in/ushatech/dsa/Stack.java
@@ -1,23 +1,44 @@
 package in.ushatech.dsa;
 
-public class Stack {
-    
-    Node top=null;
-    
-    int height=0;
-    
-    public Stack(int initialValue)
+class Stack 
+{
+    private Node peak;
+    private int length;
+
+    public Stack() {
+        this.peak = null;
+        this.length = 0;
+    }
+
+    public void push(int value) 
     {
-        top = new Node();
-        top.value=initialValue;
-        ++height;
+        Node newNode = new Node(value);
+        ++length;
+        if(peak==null)
+        {
+            peak = newNode;
+            return;
+        }
+        
+        peak.next=newNode;
+        peak=peak.next;
     }
     
-    
-    class Node 
-    {
-        Node next;
-        int value;
+    // For demonstration purposes
+    public int getPeakValue() {
+        if (peak != null) {
+            return peak.value;
+        }
+        return -1; // Return a dummy value if the stack is empty
     }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(5);
+        stack.push(10);
+        System.out.println(stack.getPeakValue());  // Outputs: 10
+    }
+
+    
 }
 //TODO : Test to be written
