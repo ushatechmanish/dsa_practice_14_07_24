493f00993978d32e5de2df6e1183b78a5462f5cf
Manish Chauhan <manish.chauhan@ushatech.in>
2024-07-15 23:20:54 +0530

    TODO ThreeInOneArrayStackImpl to be done . Task Added in the repository

---
+++ src/main/java/in/ushatech/dsa/ThreeInOneArrayStackImpl.java
@@ -0,0 +1,51 @@
+package in.ushatech.dsa;
+
+public class ThreeInOneArrayStackImpl {
+    private int numberOfStacks = 3;
+    private int stackCapacity;
+    private int[] values;
+    private int[] sizes;
+  
+    public ThreeInOneArrayStackImpl(int stackSize) 
+    {
+      
+    }
+  
+    // isFull
+    public boolean isFull(int stackNum) {
+      // TODO
+    }
+  
+    // isEmpty
+    public boolean isEmpty(int stackNum) {
+        // TODO
+    }
+  
+    // indexOfTop - this is helper method for push, pop and peek methods
+  
+    private int indexOfTop(int stackNum) 
+    {
+      // TODO
+    }
+  
+    // push
+    public void push(int stackNum, int value) 
+    {
+      // TODO
+    }
+  
+    // pop
+    public int pop(int stackNum) 
+    {
+      // TODO
+    }
+  
+    // peek
+  
+    public int peek(int stackNum) 
+    {
+      // TODO
+    }
+  
+  }
+  
