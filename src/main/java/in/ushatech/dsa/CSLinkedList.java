8e2e0971662ba12caca9ec54001c394ed69b3196
Manish Chauhan <manish.chauhan@ushatech.in>
2024-07-15 14:39:45 +0530

    deleteByValue to be checked for middle values. Learning : using index can be explored for this one.

--- src/main/java/in/ushatech/dsa/CSLinkedList.java
+++ src/main/java/in/ushatech/dsa/CSLinkedList.java
@@ -7,75 +7,120 @@ public class CSLinkedList {
     int size;
 
     public CSLinkedList() {
-        head=null;
-        tail=null;
-        size=0;
+        head = null;
+        tail = null;
+        size = 0;
     }
 
-    public void append(int value) 
-    {
-        if(size==0)
-        {
-            head=tail=getSingleNode(value);
+    public void append(int value) {
+        if (size == 0) {
+            head = tail = getSingleNode(value);
             ++size;
             return;
         }
         Node sentinel = getSentinelNode();
         Node newNode = new Node(value);
-        newNode.next=head;
-        tail.next=newNode;
-        tail=tail.next;
+        newNode.next = head;
+        tail.next = newNode;
+        tail = tail.next;
         ++size;
     }
 
-    public void prepend(int value)  // expected: <1 -> 2 -> 3> but was: <3 -> 1>
+    public void prepend(int value) // expected: <1 -> 2 -> 3> but was: <3 -> 1>
     {
-        if(size==0)
-        {
-            head=tail=getSingleNode(value);
+        if (size == 0) {
+            head = tail = getSingleNode(value);
             ++size;
             return;
         }
         Node newNode = new Node(value);
-        newNode.next=head;
+        newNode.next = head;
         tail.next = newNode;
-        head=newNode; // this was missed 
+        head = newNode; // this was missed
         ++size;
     }
 
     @Override
-    public String toString() 
-    {
+    public String toString() {
         StringBuffer sb = new StringBuffer();
         Node temp = head;
-        while(temp!=null)
-        {
+        while (temp != null) {
             sb.append(temp.value);
-            if(temp.next!=head)
-            {
+            if (temp.next != head) {
                 sb.append(" -> ");
-            }
-            else
-            {
+            } else {
                 break; // important to avoid infinite loop
             }
-            temp=temp.next;
+            temp = temp.next;
         }
         return sb.toString();
     }
-    
-    private Node getSentinelNode()
-    {
+
+    private Node getSentinelNode() {
         Node sentinel = new Node(0);
-        sentinel.next=head;
+        sentinel.next = head;
         return sentinel;
     }
-    
-    private Node getSingleNode(int value)
-    {
+
+    private Node getSingleNode(int value) {
         Node singleNode = new Node(value);
-        singleNode.next=singleNode;
+        singleNode.next = singleNode;
         return singleNode;
-        
+
     }
+
+    public boolean deleteByValue(int value) {
+        // TODO
+        throw new UnsupportedOperationException("To be checked for the middle element test ");
+        // if (size == 0)
+        // return false;
+        // if (size == 1 && head.value == value) {
+        // head = tail = null;
+        // size--;
+        // return true;
+        // }
+
+        // if (size == 1 && head.value != value) {
+        // return false;
+        // }
+
+        // // At least 2 nodes are there with head != tail
+        // if (head.value == value) {
+        // Node nodeDeleted = head;
+        // head = head.next;
+        // tail.next = head;
+        // nodeDeleted.next = null;
+        // --size;
+        // return true;
+        // }
+
+        // if (tail.value == value) {
+        // Node nodeDeleted = tail;
+        // // Reach to the prevNode to tail
+        // Node temp = head;
+        // while (temp.next != tail) {
+        // temp = temp.next;
+        // }
+        // tail = temp;
+        // tail.next = head;
+        // nodeDeleted.next = null;
+        // --size;
+        // return true;
+
+        // For middle nodes
+        // Node prevNode = head;
+        // Node current = head.next;
+        // while (current != head && current.value != value) {
+        // prevNode = current;
+        // current = current.next;
+        // }
+        // if (current == head)
+        // return false; // Value not found in the list
+
+        // prevNode.next = current.next;
+        // current.next = null;
+        // --size;
+        // return true;
+    }
+
 }
--- src/test/java/in/ushatech/dsa/CSLinkedListTest.java
+++ src/test/java/in/ushatech/dsa/CSLinkedListTest.java
@@ -3,34 +3,36 @@ package in.ushatech.dsa;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
+
 class CSLinkedListTest {
-    
+
     @Test
     void testAppend() {
         CSLinkedList list = new CSLinkedList();
         list.append(10);
         assertEquals("10", list.toString());
-        
+
         list.append(20);
         assertEquals("10 -> 20", list.toString());
-        
+
         list.append(30);
         assertEquals("10 -> 20 -> 30", list.toString());
     }
-    
+
     @Test
     void testPrepend() {
         CSLinkedList list = new CSLinkedList();
         list.prepend(10);
         assertEquals("10", list.toString());
-        
+
         list.prepend(20);
         assertEquals("20 -> 10", list.toString());
-        
+
         list.prepend(30);
         assertEquals("30 -> 20 -> 10", list.toString());
     }
-    
+
     @Test
     void testAppendAndPrepend() {
         CSLinkedList list = new CSLinkedList();
@@ -40,7 +42,7 @@ class CSLinkedListTest {
         list.prepend(1);
         assertEquals("1 -> 5 -> 10 -> 15", list.toString());
     }
-    
+
     @Test
     void testEmptyList() {
         CSLinkedList list = new CSLinkedList();
@@ -52,8 +54,59 @@ class CSLinkedListTest {
         CSLinkedList list = new CSLinkedList();
         list.append(1);
         assertEquals("1", list.toString());
-        
+
         list.prepend(2);
         assertEquals("2 -> 1", list.toString());
     }
+
+    @Test
+    @Disabled
+    public void testDeleteByValue() {
+        CSLinkedList list = new CSLinkedList();
+
+        // Case 1: Deleting from an empty list
+        assertFalse(list.deleteByValue(10), "Deleting from an empty list should return false");
+
+        // Case 2: Deleting the only element in the list
+        list.append(1);
+        assertTrue(list.deleteByValue(1), "Deleting the only element should return true");
+        assertNull(list.head, "Head should be null after deleting the only element");
+        assertNull(list.tail, "Tail should be null after deleting the only element");
+        assertEquals(0, list.size, "size should be 0 after deleting the only element");
+
+        // Case 3: Deleting an element that does not exist in a single-element list
+        list.append(1);
+        assertFalse(list.deleteByValue(2), "Deleting a non-existent element should return false");
+
+        // Case 4: Deleting the head in a multi-element list
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        assertTrue(list.deleteByValue(1), "Deleting the head should return true");
+        assertEquals(2, list.head.value, "New head should be the next element");
+        assertEquals(3, list.size, "size should be decremented after deleting the head");
+
+        // Case 5: Deleting the tail in a multi-element list
+        assertTrue(list.deleteByValue(4), "Deleting the tail should return true");
+        assertEquals(3, list.tail.value, "New tail should be the previous element");
+        assertEquals(2, list.size, "size should be decremented after deleting the tail");
+
+        // Case 6: Deleting a middle element
+        list.append(5);
+        assertTrue(list.deleteByValue(3), "Deleting a middle element should return true");
+        assertEquals(2, list.head.next.value, "The next element after head should be the next element in the list");
+        assertEquals(2, list.size, "size should be decremented after deleting a middle element");
+
+        // Case 7: Deleting a non-existent element in a multi-element list
+        assertFalse(list.deleteByValue(10), "Deleting a non-existent element should return false");
+
+        // Case 8: Check the list integrity after various operations
+        list.prepend(1);
+        list.append(3);
+        assertTrue(list.deleteByValue(2), "Deleting a middle element should return true");
+        assertEquals(1, list.head.value, "Head should be the first element");
+        assertEquals(3, list.tail.value, "Tail should be the last element");
+        assertEquals(2, list.size, "size should be correct after all deletions");
+    }
+
 }
